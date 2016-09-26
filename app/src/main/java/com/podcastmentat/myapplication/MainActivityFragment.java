46610714ebfa778a93029886ccabf665a105ab8e
Sushant <sushant.srivastav@gmail.com>
2016-09-26 05:47:59 -0400

    First cut

---
+++ app/src/main/java/com/podcastmentat/myapplication/MainActivityFragment.java
@@ -0,0 +1,52 @@
+package com.podcastmentat.myapplication;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        SimpleExoPlayerView exoplayerView = (SimpleExoPlayerView) getView().findViewById(R.id.player_view);
+        // 1. Create a default TrackSelector
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(mainHandler, videoTrackSelectionFactory);
+
+// 2. Create a default LoadControl
+        LoadControl loadControl = new DefaultLoadControl();
+
+// 3. Create the player
+        SimpleExoPlayer player =
+                ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+        exoplayerView.setPlayer(player);
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+    }
+}
